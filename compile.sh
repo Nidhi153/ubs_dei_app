bda0b8138b33ace7cc82a5a98ac7182dba7d7e2d
Nidhi153 <npshahaa@gmail.com>
2024-06-19 23:19:37 +0800

    Init commit

---
+++ compile.sh
@@ -0,0 +1,6 @@
+flutter run --dart-define=webAPIKey="AIzaSyAm1agnmTBZyWRwO09vEv-jPX-DZJ1b7hc" \
+    --dart-define=androidAPIKey="AIzaSyBHoymKMDj22kxm9klLT0L27kdbcnI5ZLw" \
+    --dart-define=iosAPIKey="AIzaSyCY97VitFZPomrAtBvLXGJn8sZDdeyVkKg" \
+    --dart-define=macOSAPIKey="AIzaSyCY97VitFZPomrAtBvLXGJn8sZDdeyVkKg" \
+    --dart-define=windowsAPIKey="AIzaSyAm1agnmTBZyWRwO09vEv" \
+    --dart-define=androidAuthKey="AIzaSyBHoymKMDj22kxm9klLT0L27kdbcnI5ZLw"
